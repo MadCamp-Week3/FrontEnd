4b74a39728adec0072f6e66969331872599b2f70
chojaework <chojaework@gmail.com>
2023-07-17 19:27:53 +0900

    Jaewon

--- src/routes/HomePage.js
+++ src/routes/HomePage.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const HomePage = () => {
   return (
     <div className='main-content'>
       <h1>Music</h1>
+      <Link to='/login'>로그인하러 가기</Link>
     </div>
     );
 };
