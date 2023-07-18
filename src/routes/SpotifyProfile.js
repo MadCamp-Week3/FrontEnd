7eca8724247b25555679de0ade6213b0f5cfca64
chojaework <chojaework@gmail.com>
2023-07-18 15:05:04 +0900

    7/18 15:05

--- src/routes/SpotifyProfile.js
+++ src/routes/SpotifyProfile.js
@@ -1,114 +1,92 @@
 import React, { useEffect, useState } from 'react';
 
 function SpotifyProfile() {
-  const [profile, setProfile] = useState(null);
-  const clientId = "8dfdcd03dc99405ea6a805c7cb932859"; // Replace with your client id
+    const [profile, setProfile] = useState(null);
+    const [playlists, setPlaylists] = useState(null);
+    const [playlistItems, setPlaylistItems] = useState(null);
+    const token = localStorage.getItem('token'); // You already have the token
 
-  useEffect(() => {
-    const params = new URLSearchParams(window.location.search);
-    const code = params.get("code");
-
-    if (!code) {
-      redirectToAuthCodeFlow(clientId);
-    } else {
-      getAccessToken(clientId, code)
-        .then(accessToken => fetchProfile(accessToken))
+    useEffect(() => {
+        fetchProfile(token)
         .then(profile => setProfile(profile));
-    }
-  }, []);
+    }, []);
 
-async function redirectToAuthCodeFlow(clientId) {
-    const verifier = generateCodeVerifier(128);
-    const challenge = await generateCodeChallenge(verifier);
-
-    localStorage.setItem("verifier", verifier);
-
-    const params = new URLSearchParams();
-    params.append("client_id", clientId);
-    params.append("response_type", "code");
-    params.append("redirect_uri", "http://localhost:3000/spotifyProfile");
-    params.append("scope", "user-read-private user-read-email");
-    params.append("code_challenge_method", "S256");
-    params.append("code_challenge", challenge);
-
-    document.location = `https://accounts.spotify.com/authorize?${params.toString()}`;
-}
-
-function generateCodeVerifier(length) {
-    let text = '';
-    let possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-
-    for (let i = 0; i < length; i++) {
-        text += possible.charAt(Math.floor(Math.random() * possible.length));
-    }
-    return text;
-}
-
-async function generateCodeChallenge(codeVerifier) {
-    const data = new TextEncoder().encode(codeVerifier);
-    const digest = await window.crypto.subtle.digest('SHA-256', data);
-    return btoa(String.fromCharCode.apply(null, [...new Uint8Array(digest)]))
-        .replace(/\+/g, '-')
-        .replace(/\//g, '_')
-        .replace(/=+$/, '');
-}
-
-
-async function getAccessToken(clientId, code) {
-    const verifier = localStorage.getItem("verifier");
-
-    const params = new URLSearchParams();
-    params.append("client_id", clientId);
-    params.append("grant_type", "authorization_code");
-    params.append("code", code);
-    params.append("redirect_uri", "http://localhost:3000/spotifyProfile");
-    params.append("code_verifier", verifier);
-
-    const result = await fetch("https://accounts.spotify.com/api/token", {
-        method: "POST",
-        headers: { "Content-Type": "application/x-www-form-urlencoded" },
-        body: params
+    async function fetchProfile(token) {
+        const result = await fetch("https://api.spotify.com/v1/me", {
+            method: "GET", headers: { Authorization: `Bearer ${token}` }
         });
-    console.log(result)
 
-    if (!result.ok) {
-        const errorResponse = await result.json();
-        console.log(errorResponse);
-        throw new Error("Failed to get access token");
-        }
+        return await result.json();
+    }  
+    
+    useEffect(() => {
+        fetchPlaylists(token)
+        .then(playlists => setPlaylists(playlists));
+    }, []);
 
-    const { access_token } = await result.json();
-    return access_token;
+    async function fetchPlaylists(token) {
+        const result = await fetch("https://api.spotify.com/v1/me/playlists", {
+            method: "GET", headers: { Authorization: `Bearer ${token}` }
+        });
+
+        return await result.json();
     }
 
-async function fetchProfile(token) {
-    const result = await fetch("https://api.spotify.com/v1/me", {
-        method: "GET", headers: { Authorization: `Bearer ${token}` }
-    });
+    async function fetchPlaylistItems(token, playlistId) {
+        const result = await fetch(`https://api.spotify.com/v1/playlists/${playlistId}/tracks`, {
+            method: "GET", headers: { Authorization: `Bearer ${token}` }
+        });
 
-    return await result.json();
-}
+        return await result.json();
+    }
 
+    const handlePlaylistClick = async (playlistId) => {
+        const items = await fetchPlaylistItems(token, playlistId);
+        setPlaylistItems(items);
+    }
 
-return (
-    <div>
-      <h1>Display your Spotify profile data</h1>
-      {profile && (
-        <section id="profile">
-          <h2>Logged in as <span>{profile.display_name}</span></h2>
-          <span>{profile.images && profile.images[0] ? profile.images[0].url : 'No profile image'}</span>
-          <ul>
-            <li>User ID: <span>{profile.id}</span></li>
-            <li>Email: <span>{profile.email}</span></li>
-            <li>Spotify URI: <a href={profile.uri}>{profile.uri}</a></li>
-            <li>Link: <a href={profile.href}>{profile.href}</a></li>
-            <li>Profile Image: <span>{profile.images && profile.images[0] ? profile.images[0].url : 'No profile image'}</span></li>
-          </ul>
-        </section>
-      )}
-    </div>
-  );
-  
+    return (
+        <div>
+            <h1>Display your Spotify profile data</h1>
+            {profile && (
+                <section id="profile">
+                    <h2>Logged in as <span>{profile.display_name}</span></h2>
+                    {profile.images && profile.images[0] && (
+                        <img src={profile.images[0].url} alt="Profile" width="300" height="300"/>
+                    )}
+                    <ul>
+                        <li>User ID: <span>{profile.id}</span></li>
+                        <li>User Country: <span>{profile.country}</span></li>
+                        <li>Email: <span>{profile.email ? profile.email : 'Email not available'}</span></li>
+                        <li>Spotify URI: <a href={profile.uri}>{profile.uri}</a></li>
+                        <li>Link: <a href={profile.href}>{profile.href}</a></li>
+                        <li>Profile Image: <span>{profile.images && profile.images[0] ? profile.images[0].url : 'No profile image'}</span></li>
+                    </ul>
+                </section>
+            )}
+            <h1>Your Spotify Playlists</h1>
+            {playlists && playlists.items.map((playlist, index) => (
+                <div key={index} onClick={() => handlePlaylistClick(playlist.id)}>
+                    <h1>{playlist.uri}</h1>
+                    <h2>{playlist.name}</h2>
+                    <p>{playlist.description}</p>
+                    <img src={playlist.images[0]?.url} alt={playlist.name} width="200" height="200"/>
+                    <a href={playlist.external_urls.spotify}>Open in Spotify</a>
+                </div>
+            ))}
+            {playlistItems && (
+                <div>
+                    <h1>Playlist Items</h1>
+                    {playlistItems.items.map((item, index) => (
+                        <div key={index}>
+                            <h2>{item.track.name}</h2>
+                            <p>{item.track.artists.map(artist => artist.name).join(', ')}</p>
+                        </div>
+                    ))}
+                </div>
+            )}
+        </div>
+    );
 }
 
 export default SpotifyProfile;
